4e5070d475e20dda9bc2afe5ddae6a324a8091e3
Yangrui <1072140597@qq.com>
2017-11-02 19:44:33 +0800

    add 创建字典

---
+++ 剑指offer/创建字典/创建字典.py
@@ -0,0 +1,14 @@
+# 创建字典的方法：
+# 1.直接创建
+dict = {'name':'earth', 'post':'80'}
+
+# 2.工厂方法
+items = [('name','earth'),('port','80')]
+dict2 = dict(items)
+dict1 = dict((['name','earth'],['port','80']))
+
+# 3.formkeys()方法
+dict1 = {}.formkeys(('x','y'),-1)
+dict = {'x':-1,'y':-1}
+dict2 = {}.formkeys(('x','y'))
+dict2 = {'x':None, 'y':None}
